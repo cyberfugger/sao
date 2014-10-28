c83096307b63e727c81f1c70bc5d5282e0038a50
alvath96 <alvathomson@yahoo.com>
2014-10-29 01:01:28 +0700

    Update insert.php
    "insert.php" has been updated and tested on ubuntu localhost. Hope it will work on the server.

--- insert.php
+++ insert.php
@@ -6,35 +6,37 @@
   $dbname = "awasist1_sao";
   $conn = mysqli_connect($servername,$username,$password,$dbname);
   
-  // uppercase the input
+  // kodeabsen to uppercase
   $kodeabsen = strtoupper($_POST['kodeabsen']);
-  
-  // check whether someone has attended the event
-  $result = mysqli_query($conn,"SELECT * FROM EventAttandance WHERE student_code = $kodeabsen");
-  
-  // if someone has attended the event
-  if ( $row = mysqli_fetch_array($result) ) {
-    header('refresh: 5; url=index.html');
-    echo 'Absen ' . $kodeabsen . ' sudah hadir jam ' .  $row['timestamp'] . '<p>';
-    echo 'Redirecting in 5 seconds ...';
+		
+	if ($kodeabsen === "" ) {
+		header('refresh: 1; url=index.html');
+		echo 'Input tidak boleh kosong!' . '<p>';
+		echo 'Redirecting in 1 seconds ...';
   } else {
-    
-    // add new record to table
-    $time_attended = date("Y-m-d H:i:s");
-    $sql = "INSERT INTO EventAttandance (timestamp, student_code) VALUES ($kodeabsen, $time_attended);";
-    
-    if ($conn->query($sql) === TRUE) {
-      header('refresh: 5; url=index.html');
-      echo 'Absen ' . $kodeabsen . ' hadir' . '<p>';
-      echo 'Redirecting in 5 seconds ...';
-    } else {
-      echo 'add record error';
+  	// check whether someone has attended the event
+  	$result = mysqli_query($conn,"SELECT * FROM EventAttendance WHERE student_code='$kodeabsen'");
+  	
+  	if ($row = mysqli_fetch_array($result)) {        
+  		header('refresh: 1; url=index.html');
+  		echo 'Absen ' . $kodeabsen . ' sudah hadir dari ' . $row['timestamp'] . '<p>';
+  		echo 'tidak berhasil memasukkan data' . '<p>'; 
+  		echo 'Redirecting in 1 seconds ...';
+  	} else {
+  	// add new record to table
+    	$sql = "INSERT INTO EventAttendance (student_code) VALUES ('$kodeabsen')";
+    	
+    	if ($conn->query($sql) === TRUE) {
+    		header('refresh: 1; url=index.html');
+    		echo 'Absen ' . $kodeabsen . ' hadir' . '<p>';
+    		echo 'Redirecting in 1 seconds ...';
+    	} else {
+    		echo 'add record error';
+    	}
     }
-    
-  }
-  // output some information then redirect
+  }  
   
-  
-  // close the connection
-  mysqli_close($conn);
+// output some information then redirect
+// close the connection
+mysqli_close($conn);
 ?>

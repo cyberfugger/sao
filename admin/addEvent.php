87a3e0245122af62d5bbda7dc98c984db60ff031
alvath96 <alvathomson@yahoo.com>
2014-10-29 16:24:06 +0700

    Update addEvent.php
    Updated 'addEvent.php' for as an event manager that accessible to admin.

--- admin/addEvent.php
+++ admin/addEvent.php
@@ -14,18 +14,55 @@
     </tr>
     <tr>
       <td>Event Password:</td>
-      <td><input type="text" name="eventPassword"/></td>
+      <td><input type="password" name="eventPassword"/></td>
     </tr>
   </table>
   <p>
-  <input type="button" value="Create" />
+  <input type="submit" value="Create" />
 </form>
 </body>
 </html>
 
 <?php
-$eventName = $_POST['eventName'];
-// $eventPassword = $_POST['eventPassword'];
+	if (isset($_POST['eventPassword']) & isset($_POST['eventName']) ) {
+		$eventName = $_POST['eventName'];
+		$eventPassword = $_POST['eventPassword'];
+		
+		$servername = "localhost";
+		$username = "awasist1";
+		$password = "awas123";
+		$dbname = "awasist1_sao";
 
-// write the code to create new SQL table here
+		$conn = new mysqli($servername,$username,$password,$dbname);
+		
+		$sql = "CREATE TABLE Events (
+					id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+					eventName VARCHAR(30) NOT NULL,
+					eventPassword VARCHAR(30) NOT NULL
+				)";
+				
+		$conn->query($sql);
+		
+		$result = mysqli_query($conn,"SELECT * FROM Events WHERE eventName='$eventName'");
+		if ($row = mysqli_fetch_array($result)) {        
+			header('refresh: 2; url=addEvent.php');
+			echo $eventName . ' sudah ada <p>';
+			echo 'tidak berhasil memasukkan data' . '<p>'; 
+			echo 'Redirecting in 2 seconds ...';
+		} else {
+			$sql = "INSERT INTO Events (eventName, eventPassword) 
+					VALUES ('$eventName','$eventPassword')";
+			
+			if ($conn->query($sql) === TRUE) {
+				 header('refresh: 2; url=addEvent.php');
+				echo 'Acara ' . $eventName . ' telah dibuat <p>';
+				echo 'Redirecting in 2 seconds ...';
+			} else {
+				echo 'add record error';
+			}
+		} 
+		
+		$conn->close();
+	} else echo 'Please input Event Name and Event Password';
+		
 ?>
